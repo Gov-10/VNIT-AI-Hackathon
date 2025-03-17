5ae0d366e23357a50f015316f29becb428abef96
Govind <govindsys1008@gmail.com>
2025-03-17 23:41:40 +0530

    Fixed ALLOWED_HOSTS

--- hackathon/hackathon/settings.py
+++ hackathon/hackathon/settings.py
@@ -195,7 +195,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000"
 ]
-CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000", 'https://your-railway-app-url.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000", 'https://vnit-ai-hackathon-production.up.railway.app/']
 
 # URL configurations
 ROOT_URLCONF = 'hackathon.urls'
